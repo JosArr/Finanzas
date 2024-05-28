de0e79824a55277c2feea10ff42aafcb38fef0d8
JosArr <josuedavidarrunategui@gmail.com>
2024-05-28 01:47:41 -0500

    create client menu

---
+++ src/app/components/client-menu/client-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-menu',
+  templateUrl: './client-menu.component.html',
+  styleUrls: ['./client-menu.component.css']
+})
+export class ClientMenuComponent {
+
+}
