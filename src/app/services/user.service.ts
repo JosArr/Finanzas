ca91fae93e4d76f51e8ebddbf9c0f084d213b6b2
JosArr <josuedavidarrunategui@gmail.com>
2024-05-29 23:56:40 -0500

    update user.service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,7 +8,7 @@ import {map, Observable} from "rxjs";
 })
 
 export class UserService {
-  private apiURL = 'https://my-json-server.typicode.com/JosArr/Finanzas/manager';
+  private apiURL = 'https://my-json-server.typicode.com/JosArr/databaseFinanzas/usuarios';
   isLoggedIn: boolean = false;
   usuarioLogueado: any;
   constructor(private http: HttpClient) { }
