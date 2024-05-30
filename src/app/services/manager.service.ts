426d4e3c03f66feeb89051a8a7d4242da330c9a9
JosArr <josuedavidarrunategui@gmail.com>
2024-05-29 23:56:18 -0500

    creation of the manager service

---
+++ src/app/services/manager.service.ts
@@ -0,0 +1,62 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {map, Observable} from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ManagerService {
+  private apiURL = 'https://my-json-server.typicode.com/JosArr/databaseFinanzas/manager';
+  isLoggedIn: boolean = false;
+  managerLogueado: any;
+  constructor(private http: HttpClient) { }
+
+  addUser(user: any): Observable<any> {
+    return this.http.post<any>(this.apiURL, user);
+  }
+
+  getManagerByEmail(email: string): Observable<any> {
+    return this.http.get<any[]>(this.apiURL).pipe(
+      map(managers => managers.find(manager => manager.correo === email))
+    );
+  }
+  editarUsuario(manager: any): Observable<any> {
+    const url = `${this.apiURL}/${manager.id}`;
+    return this.http.put<any>(url, manager);
+  }
+  getUserPassword(): Observable<string[]> {
+    return this.http.get<any[]>(this.apiURL).pipe(
+      map(managers => managers.map(manager => manager.contrasena))
+    );
+  }
+  loginUser(email: string, password: string): Observable<any> {
+    const loginData = {
+      correo: email,
+      contrasena: password,
+    };
+
+
+    return this.http.post<any>(`${this.apiURL}/login`, loginData).pipe(
+      map((response) => {
+        if (response) {
+          this.isLoggedIn = true;
+        }
+        return response;
+      })
+    );
+  }
+  cerrarSesion() {
+    this.isLoggedIn = false;
+    this.managerLogueado = null;
+  }
+  setManagerLogueado(manager: any) {
+    this.managerLogueado = manager;
+    this.isLoggedIn = true;
+  }
+  getUsuarioLogueado() {
+    return this.managerLogueado;
+  }
+
+}
