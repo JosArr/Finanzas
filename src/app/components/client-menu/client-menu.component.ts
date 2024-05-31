269cd3b6a32d9af88bb641f785c3464b45715676
JosArr <josuedavidarrunategui@gmail.com>
2024-05-30 22:12:07 -0500

    update client-menu

--- src/app/components/client-menu/client-menu.component.ts
+++ src/app/components/client-menu/client-menu.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../services/UserService/user.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-client-menu',
   templateUrl: './client-menu.component.html',
   styleUrls: ['./client-menu.component.css']
 })
-export class ClientMenuComponent {
+export class ClientMenuComponent implements OnInit {
+  ClientName: string = '';
+  correoElectronico: string =  '';
+  telefono: string = '';
+  fotoPerfil: string = '';
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {
+    const ClientLoggedIn = this.userService.getUsuarioLogueado();
+    if(ClientLoggedIn){
+      this.ClientName = ClientLoggedIn.nombre + ' ' + ClientLoggedIn.apellido;
+      this.correoElectronico = ClientLoggedIn.correo;
+      this.telefono = ClientLoggedIn.telefono;
+      this.fotoPerfil = ClientLoggedIn.perfil;
+    }
+  }
 }
