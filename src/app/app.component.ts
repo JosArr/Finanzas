5f15f3f25ed2321a399bf25d91922b5bbd0b7abd
JosArr <josuedavidarrunategui@gmail.com>
2024-05-30 22:11:24 -0500

    app update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
-import {ManagerService} from "./services/manager.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(public userService: ManagerService, private router: Router) {}
+export class AppComponent{
+  constructor(private router: Router) {}
   title = 'Bill Mind';
   isLoggedIn: boolean = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,13 +17,13 @@ import {MatPaginatorModule} from "@angular/material/paginator";
 
 import { ReactiveFormsModule } from '@angular/forms';
 import {FooterComponent} from "./public/footer/footer.component";
-import {ManagerService} from "./services/manager.service";
+import {ManagerService} from "./services/ManagerService/manager.service";
 import {IdentityScreenComponent} from "./components/identity-screen/identity-screen.component";
 import {LoginManagerComponent} from "./components/login-manager/login-manager.component";
 import {LoginClientComponent} from "./components/login-client/login-client.component";
 import {ClientMenuComponent} from "./components/client-menu/client-menu.component";
 import {ManagerMenuComponent} from "./components/manager-menu/manager-menu.component";
-import {UserService} from "./services/user.service";
+import {UserService} from "./services/UserService/user.service";
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 
