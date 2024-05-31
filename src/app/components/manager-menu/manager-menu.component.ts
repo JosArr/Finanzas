183e25f51e48896988c1839cb68d3dc4bdc2748e
JosArr <josuedavidarrunategui@gmail.com>
2024-05-30 22:13:44 -0500

    update manager-menu

--- src/app/components/manager-menu/manager-menu.component.ts
+++ src/app/components/manager-menu/manager-menu.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ManagerService} from "../../services/ManagerService/manager.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-manager-menu',
   templateUrl: './manager-menu.component.html',
   styleUrls: ['./manager-menu.component.css']
 })
-export class ManagerMenuComponent {
+export class ManagerMenuComponent implements OnInit{
 
+  ManagerName: string = '';
+  correoElectronico: string =  '';
+  telefono: string = '';
+  fotoPerfil: string = '';
+
+  constructor(private managerService: ManagerService, private router:Router) { }
+
+  menuItems=[
+    {text: 'Lista Clientes'},
+    {text: 'Modificar Información'},
+    {text: 'Reportes y estadísticas'},
+    {text: 'Cerrar Sesión'}
+  ];
+
+  ngOnInit() {
+    const ManagerLoggedIn = this.managerService.getManagerLogueado();
+    if(ManagerLoggedIn){
+      this.ManagerName = ManagerLoggedIn.nombre + ' ' + ManagerLoggedIn.apellido;
+      this.correoElectronico = ManagerLoggedIn.correo;
+      this.telefono = ManagerLoggedIn.telefono;
+      this.fotoPerfil = ManagerLoggedIn.perfil;
+    }
+  }
 }
