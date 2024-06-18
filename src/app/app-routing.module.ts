6ff657592ac6146b82ac066ec1dfd59c04d2e9dd
JosArr <josuedavidarrunategui@gmail.com>
2024-06-18 11:13:10 -0500

    delete client options

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from "@angular/router";
 import {LoginManagerComponent} from "./components/login-manager/login-manager.component";
 import {ManagerMenuComponent} from "./components/manager-menu/manager-menu.component";
 import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
+import {ClientListComponent} from "./components/client-list/client-list.component";
 
 const routes: Routes = [
   { path: 'loginManager', component: LoginManagerComponent },
   { path: 'managerMenu', component: ManagerMenuComponent },
+  { path: 'clientList', component: ClientListComponent},
   { path: '', redirectTo: 'loginManager', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
 ];
