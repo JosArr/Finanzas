0abd8e60a88b37e2af5751455558700bc320a9a7
Sebastián <u202111041@upc.edu.pe>
2024-06-24 13:39:06 -0500

    feat: new add-client

--- src/app/components/add-client/add-client.component.ts
+++ src/app/components/add-client/add-client.component.ts
@@ -8,14 +8,19 @@ import {UserService} from "../../services/UserService/user.service";
   styleUrl: './add-client.component.css'
 })
 export class AddClientComponent {
-  newClient = { nombre: '', apellido: '', telefono: '' };
+  newClient = { nombre: '', apellido: '', telefono: '' , confiabilidad: 0 };
 
   constructor(private userService: UserService, private router: Router) {}
 
   onSubmit() {
-    this.userService.addUser(this.newClient).subscribe(() => {
-      this.router.navigate(['/clientList']);
-    });
-  }
+    this.newClient.confiabilidad = Number(this.newClient.confiabilidad);
 
+    if (Number.isInteger(this.newClient.confiabilidad)) {
+      this.userService.addUser(this.newClient).subscribe(() => {
+        this.router.navigate(['/clientList']);
+      });
+    } else {
+      console.error("La confiabilidad debe ser un número entero");
+    }
+  }
 }
