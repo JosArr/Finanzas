7bb452726e2bf4ad9cdddde51364243872b733bd
JosArr <josuedavidarrunategui@gmail.com>
2024-06-24 23:53:40 -0500

    update idk

--- src/app/services/ProductService/product.service.ts
+++ src/app/services/ProductService/product.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ProductServiceService {
+export class ProductService {
 
   private apiUrl = 'https://my-json-server.typicode.com/XLianLZ/dbFinazas/products';
   constructor() { }
