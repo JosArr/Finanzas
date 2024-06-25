59a9f02502804de52e7244af45c5d62071a85b6f
Sebastián <u202111041@upc.edu.pe>
2024-06-25 02:49:14 -0500

    feat: credit-view update

--- src/app/components/credit-view/credit-view.component.ts
+++ src/app/components/credit-view/credit-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Credit} from "../../models/models/credit";
 import {CreditService} from "../../services/CreditService/credit.service";
 
@@ -7,8 +7,11 @@ import {CreditService} from "../../services/CreditService/credit.service";
   templateUrl: './credit-view.component.html',
   styleUrls: ['./credit-view.component.css']
 })
-export class CreditViewComponent {
+export class CreditViewComponent implements OnInit {
   credits: Credit[] = [];
+  selectedCredit: Credit | null = null;
+  creditLimit: number = 0;
+  annualEffectiveRate: number = 0;
 
   constructor(private creditService: CreditService) { }
 
@@ -20,5 +23,34 @@ export class CreditViewComponent {
     this.creditService.getCredits().subscribe((data: Credit[]) => {
       this.credits = data;
     });
+  }
+
+  CreditDetails(credit: Credit): void {
+    this.selectedCredit = credit;
+    this.calculateCreditDetails();
+  }
+
+  calculateCreditDetails(): void {
+    if (this.selectedCredit) {
+      this.creditLimit = this.selectedCredit.confiabilidad * 100;
+      this.annualEffectiveRate = this.calculateAnnualRate(this.selectedCredit.confiabilidad);
+    }
+  }
+
+  calculateAnnualRate(confiabilidad: number): number {
+    const tasaMinima = 8;
+    const tasaMaxima = 10;
+    return tasaMinima + ((tasaMaxima - tasaMinima) * ((5 - confiabilidad) / 4));
+  }
+
+  calculateInteresTotal(credit: Credit): number {
+    const tasaInteresMensual = ((Math.pow((1 + (credit.tasaInteres / 100)), (credit.duracionMeses / 12))) - 1) * 100;
+    const numeroPagos = credit.duracionMeses;
+    const monto = credit.monto;
+
+    const cuotaMensual = (monto * tasaInteresMensual/100) / (1 - Math.pow(1 + tasaInteresMensual/100, -numeroPagos));
+    const totalPagado = cuotaMensual * numeroPagos;
+    const InteresTotal = monto * tasaInteresMensual/100;
+    return InteresTotal;
   }
 }
